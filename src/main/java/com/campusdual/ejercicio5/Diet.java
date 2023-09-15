69baab08ed7e9bff62eb4e966f943ece28b7c212
Belial <xxdani661xx@gmail.com>
2023-09-15 14:12:06 +0200

    actualizar

---
+++ src/main/java/com/campusdual/ejercicio5/Diet.java
@@ -0,0 +1,222 @@
+package com.campusdual.ejercicio5;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+import static java.lang.System.out;
+
+public class Diet {
+    private HashMap<String,Integer[]> alimentos=new HashMap<>();
+    private Integer maxcal;
+    private Boolean women;
+    private Integer age;
+    private Integer maxCarbs;
+    private Integer maxProtein;
+    private Integer maxFats;
+    private Integer height;
+    private Integer weight;
+
+    public Diet(){
+
+    }
+    public Diet(int maxcal){
+        this.setMaxcal(maxcal);
+    }
+    public Diet(Integer maxCarbs, Integer maxProtein, Integer maxFats){
+        this.setMaxProtein(maxProtein);
+        this.setMaxCarbs(maxCarbs);
+        this.setMaxFats(maxFats);
+    }
+
+    public Diet(Boolean women, Integer age, Integer height, Integer weight){
+        this.setWomen(women);
+        this.setAge(age);
+        this.setHeight(height);
+        this.setWeight(weight);
+    }
+
+    public void add_alimentos(){
+        Scanner myObj = new Scanner(System.in);
+        String nom;
+        int carb,prot,gras;
+
+        boolean continua=false;
+        do {
+            out.println("Añadir alimento: \n");
+            out.println("\t\t-Nombre\n");
+            nom = myObj.next();
+            out.println("\t\t-Carbos\n");
+            carb=Integer.parseInt(myObj.next());
+            out.println("\t\t-Protes\n");
+            prot=Integer.parseInt(myObj.next());
+            out.println("\t\t-Grasas\n");
+            gras=Integer.parseInt(myObj.next());
+            if (check_dieta(nom,carb,prot,gras)) {
+                setAlimentos(nom, carb, prot, gras);
+                muestra_alimentos();
+            }
+            out.println("Añadir otro alimento? s/n");
+            if (myObj.next().equals("s")){
+                continua=true;
+            }else {
+                continua=false;
+            }
+        }while (continua);
+    }
+
+    public void get_dietinfo(Diet diet){
+        if(maxcal!=null){
+            out.println("\ncalorias maximas: "+maxcal);
+        }
+        if((this.getMaxFats()!=null) && (this.getMaxProtein()!=null) && (this.getMaxCarbs()!=null)) {
+            out.println("macros maximas totales: " + this.getMaxCarbs() + "carbs, " +
+                    this.getMaxProtein() + "prote," + this.getMaxFats() + "grasa");
+            out.println("calorias totales: " + this.getTotalCals());
+            out.println("macros totales actuales: " + this.getTotalCarbs() + "carbs, " +
+                    this.getTotalProt() + "prote," + this.getTotalGras() + "grasa\n");
+        }
+        if(maxcal==null) {
+            out.println("Dieta sin limite de calorias");
+        }
+        if((this.getMaxFats()!=null) && (this.getMaxProtein()!=null) && (this.getMaxCarbs()!=null) && maxcal==null) {
+            out.println("Dieta por limite de macros");
+        }
+    }
+
+    public boolean check_dieta(String nom, int c, int p, int g){
+        int newfoodcals = getFoodCals(c,p,g);
+        if(maxcal != null){
+
+            if(newfoodcals > maxcal || (getTotalCals()+newfoodcals)>maxcal){
+                out.println("te has pasado de calorias con: " + nom);
+                return false;
+            }
+        }
+        if (maxCarbs!=null && (maxCarbs<c || maxCarbs<getTotalCarbs())){
+            out.println("te has pasado de carbohidratos con: " + nom);
+            return false;
+        }
+        if (maxProtein!=null && (maxProtein<p || maxProtein<getTotalProt())){
+            out.println("te has pasado de proteinas con: " + nom);
+            return false;
+        }
+        if (maxFats!=null && (maxFats<g || maxFats<getTotalGras())){
+            out.println("te has pasado de grasas con: " + nom);
+            return false;
+        }
+        else {
+            return true;
+        }
+
+    }
+    public int getFoodCals(int c, int p, int g){
+        return (c*4)+(p*4)+(g*9);
+    }
+    public int getTotalCals(){
+        int tcals=0;
+            tcals=tcals+(getTotalCarbs()*4)+(getTotalProt()*4)+(getTotalGras()*9);
+        return tcals;
+    }
+
+    public int getTotalCarbs(){
+        int tcarbs=0;
+        for (var alim:alimentos.entrySet()){
+            tcarbs=tcarbs+alim.getValue()[0];
+        }
+        return tcarbs;
+    }
+    public int getTotalProt(){
+        int tprot=0;
+        for (var alim:alimentos.entrySet()){
+            tprot=tprot+alim.getValue()[1];
+        }
+        return tprot;
+    }
+    public int getTotalGras(){
+        int tgras=0;
+        for (var alim:alimentos.entrySet()){
+            tgras=tgras+alim.getValue()[2];
+        }
+        return tgras;
+    }
+
+
+    public void muestra_alimentos(){
+        out.println("Alimentos actuales:");
+        HashMap<String,Integer[]> ali_act = getAlimentos();
+        ali_act.forEach(
+                (key, value)
+                        -> System.out.println(key + ", valores(carbs,prot,gras):" + value[0]+","+value[1]+","+value[2]));
+    }
+    public HashMap<String, Integer[]> getAlimentos() {
+        return alimentos;
+    }
+
+    public void setAlimentos(String name, Integer carbs, Integer protes, Integer gras) {
+        this.alimentos.put(name, new Integer[]{carbs,protes,gras});
+    }
+
+    public int getMaxcal() {
+        return maxcal;
+    }
+
+    public void setMaxcal(int cal) {
+        this.maxcal = cal;
+    }
+
+    public Boolean getWomen() {
+        return women;
+    }
+
+    public void setWomen(Boolean women) {
+        this.women = women;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getMaxCarbs() {
+        return maxCarbs;
+    }
+
+    public void setMaxCarbs(Integer maxCarbs) {
+        this.maxCarbs = maxCarbs;
+    }
+
+    public Integer getMaxProtein() {
+        return maxProtein;
+    }
+
+    public void setMaxProtein(Integer maxProtein) {
+        this.maxProtein = maxProtein;
+    }
+
+    public Integer getMaxFats() {
+        return maxFats;
+    }
+
+    public void setMaxFats(Integer maxFats) {
+        this.maxFats = maxFats;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+}
---
+++ src/main/java/com/campusdual/ejercicio5/Menu.java
@@ -0,0 +1,198 @@
+package com.campusdual.ejercicio5;
+/*
+* --Escribe un programa que utilice la clase Dieta y despliegue un menú donde puedas añadir alimentos.
+* El menú tendrá las siguientes opciones.
+	-1. Crear/reiniciar dieta: crea o remplaza la dieta inicial
+		-a. Sin limite
+		-b. Por calorías
+		-c. Por macronutrientes
+		-d. Por datos personales
+	-2. Mostrar información: muestra calorías y macronutrientes de la dieta
+	-3. Agregar alimento: agrega un alimento a la dieta actual y añade ese alimento a la lista de alimentos disponible
+		-a. Nuevo alimento
+		-b. Alimento existente
+	-4. Salir
+* */
+
+import java.util.*;
+import java.util.regex.Pattern;
+
+import static java.lang.System.out;
+
+
+public class Menu {
+    public static List<String> macros = Arrays.asList("carbos", "protes", "grasas");
+    public static List<String> datos = Arrays.asList("Introduce genero(mj/h)", "Introduce edad", "Introduce altura", "Introduce  peso");
+    public static Pattern patrn = Pattern.compile("\\d+");
+    public static void main(String[] args){
+        HashMap<String, Diet> dietas =new HashMap<>();
+        Diet dieta=new Diet();
+        Scanner myObj = new Scanner(System.in);
+        String cs;
+    do{
+        out.println("-1. Crear/reiniciar dieta: crea o remplaza la dieta inicial\n" +
+                        "-2. Mostrar información: muestra calorías y macronutrientes de la dieta\n" +
+                        "-3. Agregar alimento: agrega un alimento a la dieta actual y añade ese alimento " +
+                        "a la lista de alimentos disponible\n" +
+                        "\t\t-a. Nuevo alimento\n" +
+                        "\t\t-b. Alimento existente\n" +
+                        "-4. Salir");
+        cs = myObj.nextLine();
+        String nxtline="";
+            switch (cs) {
+                case "1":
+                    out.println("Introduce el nombre de la dieta a añadir:");
+                    nxtline =myObj.nextLine();
+                    dietas.put(nxtline,get_diet_input());
+                    break;
+                case "2":
+                    show_dietas(dietas);
+                    break;
+                case "3":
+                    out.println("Introduce dieta a la que añadir o consultar alimentos:");
+                    show_dietas(dietas);
+                    nxtline =myObj.nextLine();
+                    if (patrn.matcher(nxtline).matches()) {
+                        dieta=dietas.get(nxtline);
+                    }
+                    gest_alimentos(dieta);
+                    break;
+                case "4":
+                    break;
+                default:
+                    out.println("respuesta erronea, el formato de la opcion es una letra unica, eg: '1'");
+                    break;
+
+        }
+    }while(!cs.equalsIgnoreCase("4"));
+
+    }
+
+
+    private static ArrayList<Integer> get_macros(){
+        Scanner myObj = new Scanner(System.in);
+        ArrayList<Integer> custom_macs=new ArrayList<>();
+        for (String m:macros){
+            out.println("Introduce " + m + " maximas:");
+                while(!myObj.hasNextInt()){
+                    out.println("Por favor introduce un numero valido entero:");
+                    custom_macs.add(Integer.parseInt(myObj.nextLine()));
+                }
+            custom_macs.add(Integer.parseInt(myObj.nextLine()));
+        }
+        return custom_macs;
+
+    }
+
+    private static ArrayList<String> get_data(){
+        Scanner myObj = new Scanner(System.in);
+        ArrayList<String> custom_data=new ArrayList<>();
+        for (String d:datos){
+            out.println(d +":");
+            if(d.equals("Introduce genero(mj/h)")&&(myObj.nextLine().equals("mj"))){
+                custom_data.add("true");
+            }
+            else {
+                custom_data.add(myObj.nextLine());
+            }
+        }
+        return custom_data;
+    }
+
+    private static Diet get_diet_input(){
+        Diet dieta = null;
+        Scanner myObj = new Scanner(System.in);
+        int maxcal=0;
+        String res;
+
+        do{
+            out.println("Crear/reiniciar dieta: \n" +
+                    "\t\t-a. Sin limite\n" +
+                    "\t\t-b. Por calorías\n" +
+                    "\t\t-c. Por macronutrientes\n" +
+                    "\t\t-d. Por datos personales\n" +
+                    "\t\t-s. salir\n" +
+                    "\t\t-r. reiniciar");
+
+            res = myObj.nextLine();
+            switch (res) {
+                case "a":
+                    dieta = new Diet();
+                    res="s";
+                    break;
+                case "b":
+                    do {
+                        out.println("Introduce calorias maximas:");
+                        String nxtline =myObj.nextLine();
+                        if (patrn.matcher(nxtline).matches()) {
+                            maxcal=Integer.parseInt(nxtline);
+                        }
+                    }while (maxcal==0);
+
+                    dieta = new Diet(maxcal);
+                    out.println("cals maximas "+dieta.getMaxcal());
+                    res="s";
+                    break;
+                case "c":
+                    ArrayList<Integer> result_macros = get_macros();
+                    dieta = new Diet(result_macros.get(0), result_macros.get(1), result_macros.get(2));
+                    res="s";
+                    break;
+                case "d":
+                    ArrayList<String> result_data = get_data();
+                    dieta = new Diet(Boolean.parseBoolean(result_data.get(0)), Integer.parseInt(result_data.get(1)),
+                            Integer.parseInt(result_data.get(2)), Integer.parseInt(result_data.get(3)));
+                    res="s";
+                    break;
+                case "r":
+                    dieta=new Diet();
+                    break;
+                case "s":
+                    dieta=new Diet();
+                    break;
+                default:
+                    out.println("respuesta erronea, el formato de la opcion es una letra unica, eg: 'a'");
+                    break;
+            }
+
+        }while(!res.equalsIgnoreCase("s"));
+
+        return dieta;
+    }
+
+    private static void gest_alimentos(Diet dieta){
+        Scanner myObj = new Scanner(System.in);
+        out.println("Introduce operacion a realizar:"+
+                "\t\t-a. Nuevo alimento\n" +
+                "\t\t-b. Alimento existente\n");
+        String nxtline =null;
+        do{
+            nxtline =myObj.nextLine();
+            switch (nxtline) {
+                case "a":
+                    dieta.add_alimentos();
+                    break;
+                case "b":
+                    dieta.muestra_alimentos();
+                    break;
+                default:
+                    out.println("respuesta erronea, el formato de la opcion es una letra unica, eg: 'a'");
+                    break;
+            }
+            out.println("hacer otra tarea? s/n");
+            nxtline =myObj.nextLine();
+
+        }while(nxtline.equalsIgnoreCase("s"));
+    }
+
+    public static void show_dietas(HashMap<String, Diet> dietas){
+        out.println("Estas son las dietas actuales:");
+            dietas.forEach((clave,valor) ->{
+                out.println("Dieta: "+ clave);
+                out.println("valores:");
+                valor.get_dietinfo(valor);
+            });
+            }
+
+}
+
